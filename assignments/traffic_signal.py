f4fc902bfc2956f69c4dd6dcde0bfc5f6555d38a
amalarajarapu <amalarajarapu25@gmail.com>
2026-09-01 14:28:29 +0530

    Add traffic signal response logic

--- assignments/traffic_signal.py
+++ assignments/traffic_signal.py
@@ -1,1 +1,15 @@
+signal = input("Enter traffic signal color (Red/Yellow/Green): ")
 
+signal = signal.lower()
+
+if signal == "red":
+    print("STOP - Do not move.")
+
+elif signal == "yellow":
+    print("WAIT - Get ready to stop.")
+
+elif signal == "green":
+    print("GO - You can move.")
+
+else:
+    print("Invalid signal color.")
